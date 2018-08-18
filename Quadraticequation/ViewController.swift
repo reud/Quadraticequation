974e8a9e5cd81e13f4d524e3540bc8d951f1b3ed
reiji <31179220+reud@users.noreply.github.com>
2018-08-18 10:11:21 +0900

    Initial Commit

---
+++ Quadraticequation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quadraticequation
+//
+//  Created by reiji kobayashi on 2018/08/18.
+//  Copyright © 2018年 reud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
